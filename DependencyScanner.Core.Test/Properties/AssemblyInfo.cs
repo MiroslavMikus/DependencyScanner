752fb2aca22ab323531a068bde199816640f0260
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-07-15 22:22:52 +0200

    Init scan class

---
+++ DependencyScanner.Core.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DependencyScanner.Core.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DependencyScanner.Core.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("50d8bd5b-afc2-4c5b-91d3-9aadf164c8d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DependencyScanner.Core.Test/TempTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DependencyScanner.Core.Test
+{
+    [TestClass]
+    public class TempTest
+    {
+        [TestMethod]
+        public void TempTesting()
+        {
+            var scan = new Scanner();
+
+            var result = scan.Scan(@"F:\Projects\_GitHub\Exercise.DynamicProxy");
+
+
+        }
+    }
+}
