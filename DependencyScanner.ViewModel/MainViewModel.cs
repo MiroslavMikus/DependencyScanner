8ec6d3b31bb805532f4bbcb9fbe4e9f5c56ccd6f
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-07-21 10:53:12 +0200

    Add standalone with mahapps & add view model with mvvm light

---
+++ DependencyScanner.ViewModel/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyScanner.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public BrowseViewModel BrowseVM { get; set; }
+        public ConsolidateSolutionsViewModel ConsolidateSolutionsVM{ get; set; }
+    }
+
+    public class BrowseViewModel : ViewModelBase
+    {
+        public RelayCommand PickWorkingDirectoryCommand { get; private set; }
+
+        public FileInfo WorkingDirectory { get; set; }
+    }
+
+    public class ConsolidateSolutionsViewModel : ViewModelBase
+    {
+        public RelayCommand ScanCommand { get; private set; }
+    }
+
+    public class ConsolidateProjects : ViewModelBase
+    {
+        public RelayCommand ScanCommand { get; private set; }
+    }
+}
