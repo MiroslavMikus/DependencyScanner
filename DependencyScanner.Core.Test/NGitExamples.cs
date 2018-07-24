bf77aa63ef029e2e9dd6201aef430399c13d4331
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-07-24 20:14:05 +0200

    add NGit_GetBranch

---
+++ DependencyScanner.Core.Test/NGitExamples.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NGit;
+using NGit.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyScanner.Core.Test
+{
+    [TestClass]
+    public class NGitExamples
+    {
+        [TestMethod]
+        public void NGit_GetBranch()
+        {
+            var git = Git.Open(@"F:\Projects\_GitHub\DependencyScanner");
+
+            ListBranchCommand command = git.BranchList();
+
+            var result = command.Call();
+        }
+    }
+}
