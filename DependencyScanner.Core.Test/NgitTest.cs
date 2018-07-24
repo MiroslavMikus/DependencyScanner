09489b7afe3e4a08b226352cd76cb3e3334e5b75
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-07-24 20:08:16 +0200

    Add Ngit test

---
+++ DependencyScanner.Core.Test/NgitTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NGit;
+using NGit.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyScanner.Core.Test
+{
+    [TestClass]
+    public class NgitTest
+    {
+        [TestMethod]
+        public void Ngit_Test()
+        {
+            var repo = Git.Open(@"F:\Projects\_GitHub\DependencyScanner").GetRepository();
+            //ListBranchCommand
+            var test = repo.GetBranch();
+            var test1 = repo.GetConfig();
+            var test2 = repo.GetRepositoryState();
+            var test3 = repo.GetFullBranch();
+        }
+    }
+}
