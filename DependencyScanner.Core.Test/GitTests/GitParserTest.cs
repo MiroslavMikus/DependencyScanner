d781dcdb08eef9a65286b7074fa5eb6293266dc5
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-07-26 18:43:31 +0200

    Add GitManager

---
+++ DependencyScanner.Core.Test/GitTests/GitParserTest.cs
@@ -0,0 +1,43 @@
+﻿using DependencyScanner.Core.GitClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+namespace DependencyScanner.Core.Test.GitTests
+{
+    [TestClass]
+    public class GitParserTest
+    {
+        private string _exampleBranchListInput = "  fix/SyncWithUi\r\n* master\r\n";
+
+        [TestMethod]
+        public void Parser()
+        {
+            var processResult = GitEngine.GitProcess(Environment.CurrentDirectory, GitCommand.BranchList);
+            var result = GitParser.SplitString(processResult);
+
+            AreEqual("  fix/SyncWithUi", result.First());
+            AreEqual("* master", result.Last());
+        }
+
+        [TestMethod]
+        public void GetBranchList_Test()
+        {
+            var result = GitParser.GetBranchList(_exampleBranchListInput);
+
+            AreEqual("fix/SyncWithUi", result.First());
+            AreEqual("master", result.Last());
+        }
+
+        [TestMethod]
+        public void GetMaster_Test()
+        {
+            var result = GitParser.GetCurrentBranch(_exampleBranchListInput);
+
+            AreEqual("master", result);
+        }
+    }
+}
---
+++ DependencyScanner.Core/GitClient/GitManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyScanner.Core.GitClient
+{
+    public class GitManager
+    {
+
+    }
+
+    public class GitParser
+    {
+        public static IEnumerable<string> GetBranchList(string input)
+        {
+            var arr = SplitString(input);
+
+            return arr.Select(a => a.Substring(2));
+        }
+
+        public static string GetCurrentBranch(string input)
+        {
+            var arr = SplitString(input);
+
+            return arr.First(a => a.StartsWith("*")).Substring(2);
+        }
+
+        public static IEnumerable<string> SplitString(string input) => input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).Where(a => !string.IsNullOrEmpty(a));
+    }
+
+    public static class GitEngine
+    {
+        internal static string GitProcess(string workingDirectory, string parameter)
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    WorkingDirectory = workingDirectory,
+                    FileName = "git",
+                    Arguments = parameter,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+
+            StringBuilder sb = new StringBuilder();
+
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                sb.AppendLine(proc.StandardOutput.ReadLine());
+            }
+            return sb.ToString();
+        }
+    }
+
+    public static class GitCommand
+    {
+        public const string BranchList = "branch";
+        public const string Status = "status";
+        public const string SwitchBranch = "checkout";
+    }
+}
