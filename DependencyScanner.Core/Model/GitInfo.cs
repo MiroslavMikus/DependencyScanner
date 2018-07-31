4e3ca8e8213392eefb4cbebff1cfb4d6253e4fb6
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-07-31 08:59:13 +0200

    add pull button

--- DependencyScanner.Core/Model/GitInfo.cs
+++ DependencyScanner.Core/Model/GitInfo.cs
@@ -1,19 +1,52 @@
 ﻿using DependencyScanner.Core.GitClient;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DependencyScanner.Core.Model
 {
-    public class GitInfo
+    public class GitInfo : ObservableObject
     {
         public FileInfo Root { get; }
-        public string CurrentBranch { get; private set; }
-        public string Status { get; private set; }
         public string RemoteUrl { get; private set; }
         public IEnumerable<string> BranchList { get; private set; }
         public bool IsClean { get => Status.Contains("working tree clean"); }
         public bool IsBehind { get => Status.Contains("Your branch is behind"); }
+
+        private string _currentBranch;
+        public string CurrentBranch
+        {
+            get => _currentBranch;
+            set
+            {
+                if (Set(ref _currentBranch, value))
+                {
+                    Task.Run(() => 
+                    {
+                        Checkout(value);
+                        Status = GitEngine.GitProcess(Root.DirectoryName, GitCommand.Status);
+                    });
+                }
+            }
+        }
+
+        private string _status;
+        public string Status
+        {
+            get => _status;
+            private set
+            {
+                Set(ref _status, value);
+
+                RaisePropertyChanged(nameof(IsClean));
+                RaisePropertyChanged(nameof(IsBehind));
+            }
+        }
+
+        public RelayCommand PullCommand { get; }
 
         public GitInfo(string root)
         {
@@ -28,11 +61,20 @@ namespace DependencyScanner.Core.Model
 
             BranchList = GitParser.GetBranchList(branches);
 
-            CurrentBranch = GitParser.GetCurrentBranch(branches);
+            _currentBranch = GitParser.GetCurrentBranch(branches);
 
             Status = GitEngine.GitProcess(Root.DirectoryName, GitCommand.Status);
 
             RemoteUrl = GitEngine.GitExecute(Root.DirectoryName, GitCommand.RemoteBranch, a => GitParser.GetRemoteUrl(a));
+
+            PullCommand = new RelayCommand(() =>
+            {
+                Task.Run(() =>
+                {
+                    Pull();
+                    Status = GitEngine.GitProcess(Root.DirectoryName, GitCommand.Status);
+                });
+            });
         }
 
         public string Checkout(string branch)
@@ -41,6 +83,7 @@ namespace DependencyScanner.Core.Model
             {
                 return GitEngine.GitProcess(Root.DirectoryName, GitCommand.SwitchBranch, branch);
             }
+
             return $"Branch '{branch}' doesnt exist in scan results!";
         }
 
