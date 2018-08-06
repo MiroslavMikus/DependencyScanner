454e810f832b464cead406cfa02a56111779c86a
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-08-06 10:42:21 +0200

    Add verification

--- DependencyScanner.Standalone/Properties/AssemblyInfo.cs
+++ DependencyScanner.Standalone/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("DependencyScanner.Standalone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Miroslav Mikus")]
 [assembly: AssemblyProduct("DependencyScanner.Standalone")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
