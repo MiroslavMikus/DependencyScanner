82f0570649a4c002df3b32c6a11a9396c3af9e91
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2018-08-09 13:02:06 +0200

    Add choco updater class

---
+++ DependencyScanner.Standalone/ChocoUpdater.cs
@@ -0,0 +1,64 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyScanner.Standalone
+{
+    public class ChocoUpdater
+    {
+        public const string PackageId = "dependency-scanner";
+
+        public bool IsNewVersionAvailable()
+        {
+            var search = SearchInChoco();
+
+            return IsOutdated(search);
+        }
+
+        private bool IsOutdated(string input)
+        {
+            var lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).Where(a => !string.IsNullOrEmpty(a));
+
+            foreach (var line in lines)
+            {
+                if( line.IndexOf(PackageId, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private string SearchInChoco()
+        {
+            Log.Debug("Checking latest version");
+
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "choco",
+                    Arguments = "outdated",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+
+            StringBuilder sb = new StringBuilder();
+
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                sb.AppendLine(proc.StandardOutput.ReadLine());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
