a5048cceb1fb5f3dc2c03fe1d43e920407964e7a
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-09-04 22:17:24 +0200

    update chocoUpdater

--- DependencyScanner.Standalone/ChocoUpdater.cs
+++ DependencyScanner.Standalone/ChocoUpdater.cs
@@ -26,14 +26,16 @@ namespace DependencyScanner.Standalone
         {
             var lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).Where(a => !string.IsNullOrEmpty(a));
 
-            foreach (var line in lines)
-            {
-                if (line.IndexOf(PackageId, StringComparison.OrdinalIgnoreCase) >= 0)
-                {
-                    return true;
-                }
-            }
-            return false;
+            return lines.Any(a => a.IndexOf(PackageId, StringComparison.OrdinalIgnoreCase) >= 0);
+
+            //foreach (var line in lines)
+            //{
+            //    if (line.IndexOf(PackageId, StringComparison.OrdinalIgnoreCase) >= 0)
+            //    {
+            //        return true;
+            //    }
+            //}
+            //return false;
         }
 
         private async Task<string> SearchInChoco()
