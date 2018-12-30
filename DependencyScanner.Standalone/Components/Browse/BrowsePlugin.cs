639888c5ca6fd4b085f22ad0c7acdf6a5a9b597e
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-12-30 14:06:07 +0100

    Refactor Browse tab

---
+++ DependencyScanner.Standalone/Components/Browse/BrowsePlugin.cs
@@ -0,0 +1,30 @@
+﻿using DependencyScanner.Core.Interfaces;
+using DependencyScanner.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DependencyScanner.Standalone.Components.Browse
+{
+    public class BrowsePlugin : IPlugin
+    {
+        public string Title => "Browse";
+
+        public string Description => "Scan and browse your solutions.";
+
+        public UserControl ContentView { get; }
+
+        public int Order => 1;
+
+        public BrowsePlugin(BrowseViewModel browseViewModel)
+        {
+            ContentView = new BrowseView
+            {
+                DataContext = browseViewModel
+            };
+        }
+    }
+}
