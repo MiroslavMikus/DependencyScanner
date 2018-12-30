f3dcde1ccea4355ebcbff3ce0b621d7d0bc219a8
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-12-30 14:02:44 +0100

    Add IPlugin Interface

---
+++ DependencyScanner.Core/Interfaces/IPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace DependencyScanner.Core.Interfaces
+{
+    public interface IPlugin
+    {
+        string Title { get; }
+        string Description { get; }
+        UserControl ContentView { get; }
+        int Order { get; }
+        //Page Settings { get; }
+        //Page Help { get; }
+    }
+}
