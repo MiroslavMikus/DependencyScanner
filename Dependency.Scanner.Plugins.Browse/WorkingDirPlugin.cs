0938f8049e57c73937bb0378834295e38866a0d5
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-01-12 22:21:59 +0100

    Add fody to working directory plugin

---
+++ Dependency.Scanner.Plugins.Browse/WorkingDirPlugin.cs
@@ -0,0 +1,21 @@
+﻿using DependencyScanner.Api.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Dependency.Scanner.Plugins.Browse
+{
+    public class WorkingDirPlugin : IPlugin
+    {
+        public string Title => "Working directories";
+
+        public string Description => "Organize and browse your working directories";
+
+        public UserControl ContentView => new WorkingDirView();
+
+        public int Order => 0;
+    }
+}
