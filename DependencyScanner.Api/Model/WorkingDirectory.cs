62bcbb892d14122540bac15f7d53d6d329b0fd78
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-01-12 17:09:53 +0100

    update api model

--- DependencyScanner.Api/Model/WorkingDirectory.cs
+++ DependencyScanner.Api/Model/WorkingDirectory.cs
@@ -1,34 +1,93 @@
 ﻿using DependencyScanner.Api.Interfaces;
-using System;
+using DependencyScanner.Api.Services;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using GalaSoft.MvvmLight.Threading;
+using Serilog;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Input;
 
 namespace DependencyScanner.Api.Model
 {
-    public class WorkingDirectory : IScannable
+    public class WorkingDirectory : ObservableObject, ISyncable
     {
-        public ICollection<Repository> Solutions { get; set; }
+        private string _path;
+        private readonly ILogger _logger;
+        private readonly IRepositoryScanner _scanner;
+        private readonly IMessenger _messenger;
+        private CancellationTokenSource _cancellationTokenSource;
 
-        public void Sync(IScanner scanner)
+        public string Path { get => _path; set => Set(ref _path, value); }
+
+        public ICollection<Repository> Repositories { get; set; } = new ObservableCollection<Repository>();
+        public ICommand PullCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public WorkingDirectory(ILogger logger, IRepositoryScanner scanner, IMessenger messenger)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+            _scanner = scanner;
+            _messenger = messenger;
+
+            PullCommand = new RelayCommand(async () =>
+            {
+                _cancellationTokenSource = new CancellationTokenSource();
+
+                var progress = new DefaultProgress
+                {
+                    Token = _cancellationTokenSource.Token
+                };
+
+                // todo implement progress
+
+                var repos = await _scanner.ScanForGitRepositories(_path, progress);
+
+                DispatcherHelper.CheckBeginInvokeOnUI(() =>
+                {
+                    Repositories = new ObservableCollection<Repository>(repos.Select(a => new Repository(a)));
+
+                    //_messenger.Send<WorkingDirectory>(this);
+                });
+            });
+
+            CancelCommand = new RelayCommand(() =>
+            {
+                _cancellationTokenSource?.Cancel();
+            });
         }
     }
 
-    public class Repository : IScannable
+    public class Repository : ISyncable
     {
-        public ICollection<Project> Projects { get; set; }
+        private CancellationTokenSource _cancellationTokenSource;
 
-        public void Sync(IScanner scanner)
+        public IGitInfo GitInfo { get; set; }
+        public ICommand PullCommand { get; }
+        public ICommand CancelCommand { get; }
+
+        public Repository(IGitInfo gitInfo)
         {
-            throw new NotImplementedException();
+            GitInfo = gitInfo;
+
+            PullCommand = new RelayCommand(() =>
+            {
+                GitInfo.Pull();
+            });
+
+            CancelCommand = new RelayCommand(() =>
+            {
+                _cancellationTokenSource?.Cancel();
+            });
         }
     }
 
     public class Solution
     {
+        public ICollection<Project> Projects { get; set; }
     }
 
     public class Project
---
+++ DependencyScanner.Api/Services/DefaultProgress.cs
@@ -0,0 +1,17 @@
+﻿using DependencyScanner.Api.Interfaces;
+using System;
+using System.Threading;
+
+namespace DependencyScanner.Api.Services
+{
+    public class DefaultProgress : ICancelableProgress<ProgressMessage>
+    {
+        public Action<ProgressMessage> ReportAction { get; set; }
+        public CancellationToken Token { get; set; }
+
+        public void Report(ProgressMessage value)
+        {
+            ReportAction?.Invoke(value);
+        }
+    }
+}
