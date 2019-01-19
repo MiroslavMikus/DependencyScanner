0d705a89369eddeb09d634a3e8f1c38b03242e57
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-01-19 22:42:49 +0100

    add WorkingDirectoryModule

---
+++ DependencyScanner.Plugins.WorkingDirectory/WorkingDirectoryModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace Dependency.Scanner.Plugins.Wd
+{
+    public class WorkingDirectoryModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<WorkingDirectoryViewModel>();
+        }
+    }
+}
