30a92d618952fb33a9d47faf5a2e92f9d22b1a89
Mikus Miroslav <miroslav.mikus@outlook.com>
2019-02-04 06:15:05 +0100

    Add abstract type to bool converter

---
+++ DependencyScanner.Core.Gui/Converters/AbstractTypeToBoolConvereter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DependencyScanner.Core.Gui.Converters
+{
+    public class AbstractTypeToBoolConvereter<T> : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is T;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
