c2113775f0f0b9444670832eded7519930229919
Miroslav Mikus <miroslav.mikus@outlook.com>
2019-03-03 21:34:50 +0100

    Add new Converters

---
+++ DependencyScanner.Core.Gui/Converters/AbstractEqualConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DependencyScanner.Core.Gui.Converters
+{
+    public abstract class AbstractEqualConverter<T> : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value?.Equals(parameter) ?? false)
+            {
+                return Positive;
+            }
+            return Negative;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract T Positive { get; set; }
+
+        public abstract T Negative { get; set; }
+    }
+}
---
+++ DependencyScanner.Core.Gui/Converters/BoolAndMultiValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace DependencyScanner.Core.Gui.Converters
+{
+    public class BoolAndMultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.All(a => a is bool input && input);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
