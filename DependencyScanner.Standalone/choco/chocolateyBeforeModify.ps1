bff9b68def98cfeb13b6266bc7ce7ef0ae4bd996
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2019-03-05 22:09:42 +0100

    add chocolateyBeforModify script

---
+++ DependencyScanner.Standalone/choco/chocolateyBeforeModify.ps1
@@ -0,0 +1,9 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+
+$shortcut = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'DependencyScanner.Standalone.lnk'
+$desktopShortcut = Join-Path ([System.Environment]::GetFolderPath('Desktop')) 'DependencyScanner.Standalone.lnk'
+
+Remove-Item $shortcut
+Remove-Item $desktopShortcut
+
+$toos > C:\temp2\test.txt
