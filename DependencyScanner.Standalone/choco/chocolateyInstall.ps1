5f6c977ece68f07672e988c122d9daed25e74b1c
Miroslav Mikus <Miroslav.Mikus@outlook.com>
2019-03-05 22:39:11 +0100

    update chocolateyInstall script

--- DependencyScanner.Standalone/choco/chocolateyInstall.ps1
+++ DependencyScanner.Standalone/choco/chocolateyInstall.ps1
@@ -1,15 +1,18 @@
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 
+$toolsParent = (Get-Item (Split-Path -parent $MyInvocation.MyCommand.Definition)).Parent.FullName
+
 $shortcut = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'DependencyScanner.Standalone.lnk'
 $desktopShortcut = Join-Path ([System.Environment]::GetFolderPath('Desktop')) 'DependencyScanner.Standalone.lnk'
 
 $exe = Join-Path (Split-Path $tools) 'DependencyScanner.Standalone.exe'
 
-
 Install-ChocolateyShortcut `
     -ShortcutFilePath $shortcut `
-    -TargetPath $exe
+    -TargetPath $exe `
+	-WorkingDirectory $toolsParent
 
 Install-ChocolateyShortcut `
     -ShortcutFilePath $desktopShortcut `
-    -TargetPath $exe
+    -TargetPath $exe `
+	-WorkingDirectory $toolsParent
