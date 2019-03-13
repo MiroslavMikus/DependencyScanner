68201fec627ac6f7c29bd42a1b3d8debf6eff1a0
Miroslav Mikus <miroslav.mikus@outlook.com>
2019-03-13 06:53:00 +0100

    Add dashboard element to settings

--- DependencyScanner.Plugins.WorkingDirectory/Model/WorkingDirectorySettings.cs
+++ DependencyScanner.Plugins.WorkingDirectory/Model/WorkingDirectorySettings.cs
@@ -10,5 +10,29 @@ namespace DependencyScanner.Plugins.Wd.Model
         public string Id => "WorkingDirectorySettings";
         public List<StorableWorkingDirectory> WorkingDirectoryStructure { get; set; } = new List<StorableWorkingDirectory>();
         public bool ExecuteGitFetchWhileScanning { get; set; } = true;
+        //public List<DashboardElement> Dashboards { get; set; }
+    }
+
+    public class DashboardElement
+    {
+        public int Order { get; set; }
+        public string Title { get; set; }
+        public List<DashboardLine> Lines { get; set; }
+    }
+
+    public class DashboardLine
+    {
+        public string Process { get; set; }
+        public string StringFormat { get; set; }
+    }
+
+    public class ButtonData
+    {
+        public int Order { get; set; }
+        public string ToolTip { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public string Process { get; set; }
+        public bool RefreshDashboard { get; set; }
     }
 }
---
+++ DependencyScanner.Plugins.WorkingDirectory/Services/DashboardExec.cs
@@ -0,0 +1,29 @@
+﻿using DependencyScanner.Plugins.Wd.Model;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyScanner.Plugins.Wd.Services
+{
+    public class DashboardExec : ViewModelBase
+    {
+        private readonly DashboardElement _dashboardElement;
+        public ObservableCollection<string> DashboardLine { get; set; } = new ObservableCollection<string>();
+
+        public DashboardExec(DashboardElement dashboardElement)
+        {
+            _dashboardElement = dashboardElement;
+
+            Task.Run(async () =>
+            {
+                foreach (var item in dashboardElement.Lines)
+                {
+                }
+            });
+        }
+    }
+}
